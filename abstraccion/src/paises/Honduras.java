6d049c7eadb71677f559440aed6a26f2ac0f22ea
Lilian2020-01 <62166751+Lilian2020-01@users.noreply.github.com>
2020-03-19 01:14:21 -0600

    Add files via upload

---
+++ abstraccion/src/paises/Honduras.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paises;
+
+/**
+ *
+ * @author RUBEN
+ */
+public class Honduras extends pais{
+
+public String getpais(){
+  return "Honduras";
+}
+public String getpresidente(){
+   return "Juan Orlando";
+}
+    
+}
---
+++ abstraccion/src/paises/Mexico.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paises;
+
+/**
+ *
+ * @author RUBEN
+ */
+public class Mexico extends pais{
+
+public String getpais(){
+  return "Mexico";
+  
+}
+public String getpresidente(){
+   return "Lopez Obrador";
+}
+    
+}
